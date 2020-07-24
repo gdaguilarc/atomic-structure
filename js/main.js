42725f59cd99d3cf59f9f8deeae1783f128e9270
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2020-07-23 20:32:25 -0500

    Calculating delta time

--- js/main.js
+++ js/main.js
@@ -1,34 +1,39 @@
 class Main {
 
+
     constructor(canvas) {
+        this.previousTimestamp = 0;
         // RENDERER ENGINE
         this.canvas = canvas;
-        this.engine = new THREE.WebGLRenderer({ canvas });
-        this.engine.setSize(window.innerWidth, window.innerHeight);
-        // SCENE
-        this.scene = new ADScene(canvas);
+        this.context = canvas.getContext('2d');
+        // this.engine = new THREE.WebGLRenderer({ canvas });
+        // this.engine.setSize(window.innerWidth, window.innerHeight);
+        // // SCENE
+        // this.scene = new ADScene(canvas);
 
-        // This makes the calling context always equal to the instance
+        // // This makes the calling context always equal to the instance
         this.run = this.run.bind(this);
         this.resize = this.resize.bind(this);
     }
 
 
-    run() {
+    run(timestamp) {
+        // Make animations time dependent
+        const delta = (timestamp - this.previousTimestamp) / 1000;
         // ACTION
-        this.render();
+        this.render(delta);
         requestAnimationFrame(this.run);
+        this.previousTimestamp = timestamp;
     }
 
-    render() {
-        this.engine.render(this.scene, this.scene.camera);
+    render(delta) {
     }
 
     resize() {
         this.canvas.height = window.innerHeight;
         this.canvas.width = window.innerWidth;
-        this.engine.setSize(this.canvas.width, this.canvas.height);
-        this.scene.resize();
+        // this.engine.setSize(this.canvas.width, this.canvas.height);
+        // this.scene.resize();
     }
 
 }
