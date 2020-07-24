4178b9589810f44b5945d898b1256c15f9facb0a
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2020-07-23 20:35:07 -0500

    Rollback to 3d context

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ class Main {
         this.previousTimestamp = 0;
         // RENDERER ENGINE
         this.canvas = canvas;
-        this.context = canvas.getContext('2d');
-        // this.engine = new THREE.WebGLRenderer({ canvas });
-        // this.engine.setSize(window.innerWidth, window.innerHeight);
-        // // SCENE
-        // this.scene = new ADScene(canvas);
+        this.engine = new THREE.WebGLRenderer({ canvas });
+        this.engine.setSize(window.innerWidth, window.innerHeight);
 
-        // // This makes the calling context always equal to the instance
+        // SCENE
+        this.scene = new ADScene(canvas);
+
+        // This makes the calling context always equal to the instance
         this.run = this.run.bind(this);
         this.resize = this.resize.bind(this);
     }
@@ -20,20 +20,22 @@ class Main {
     run(timestamp) {
         // Make animations time dependent
         const delta = (timestamp - this.previousTimestamp) / 1000;
+
         // ACTION
         this.render(delta);
-        requestAnimationFrame(this.run);
         this.previousTimestamp = timestamp;
+        requestAnimationFrame(this.run);
     }
 
     render(delta) {
+        this.engine.render(this.scene, this.scene.camera);
     }
 
     resize() {
         this.canvas.height = window.innerHeight;
         this.canvas.width = window.innerWidth;
-        // this.engine.setSize(this.canvas.width, this.canvas.height);
-        // this.scene.resize();
+        this.engine.setSize(this.canvas.width, this.canvas.height);
+        this.scene.resize();
     }
 
 }
